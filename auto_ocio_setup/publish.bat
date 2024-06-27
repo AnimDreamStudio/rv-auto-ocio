33c5ba5961c23c431f7c7c5261c91783229974e9
zen-TD <99463816@qq.com>
2024-06-28 01:49:45 +0800

    升级版本号

--- auto_ocio_setup/publish.bat
+++ auto_ocio_setup/publish.bat
@@ -1,3 +1,3 @@
-zip auto_ocio_setup-1.1.rvpkg auto_ocio_setup.py PACKAGE
-xcopy auto_ocio_setup-1.1.rvpkg .. /Y
+zip auto_ocio_setup-1.2.rvpkg auto_ocio_setup.py PACKAGE
+xcopy auto_ocio_setup-1.2.rvpkg .. /Y
 xcopy auto_ocio_setup.py ..\..\Python\ /Y
